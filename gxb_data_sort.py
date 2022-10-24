4a9c92d6df54ff4ff6cf05a7961e5188abe33682
thianda <thianda91@outlook.com>
2022-10-24 16:59:49 +0800

    v0.7

--- gxb_data_sort.py
+++ gxb_data_sort.py
@@ -9,13 +9,14 @@ from pandas import read_excel, concat, DataFrame
 import time
 from pathlib import Path
 import warnings
+import os
 # import openpyxl
 # import numpy
 import traceback
 from sys import exit
 
 __title__ = ''.join(chr(x) for x in [24037, 20449, 37096, 32593, 31449, 39640, 21361, 23458, 25143, 25968, 25454, 25972, 29702, 23567, 24037, 20855])
-__version__ = 'v0.6'
+__version__ = 'v0.7'
 __author__ = ''.join(chr(x) for x in [20110, 26174, 36798, 46, 38081, 23725])
 
 DEBUG_FILE = 'debug_log.txt'
@@ -36,14 +37,28 @@ else:
     print('配置文件“ {} ”不存在，无法运行。'.format(configfile))
     exit()
 
-col_names = config.get('common', 'col_names').split('\t')
-
-INDEX = config.get('common', 'index')
-FIRST_TIME = config.get('common', 'first_time')
-LAST_TIME = config.get('common', 'last_time')
+COL_NAME_TIME = config.get('common', 'col_name_time')
+PROVINCE = config.get('common', 'province')
+COL_NAMES = config.get('common', 'col_names').split('\t')
+keywords = ['index', 'first_time', 'last_time', 'count']
+INDEX = config.get('common', keywords[0])
+FIRST_TIME = config.get('common', keywords[1])
+LAST_TIME = config.get('common', keywords[2])
+COUNT = config.get('common', keywords[3])
 READ_SHEET_NAME = config.get('common', 'read_sheet_name')
 BOOK_SHEET_NAME = config.get('common', 'book_sheet_name')
+BACKUP_MODE = config.get('common', 'backup_mode')
 
+def check_config() -> bool:
+    '''
+    运行前检查配置文件参数是否存在
+    '''
+    flag = True
+    for i, v in enumerate([INDEX, FIRST_TIME, LAST_TIME, COUNT]):
+        if not v in COL_NAMES:
+            print(Fore.LIGHTRED_EX + '无法正确运行，请检查配置文件 col_names 字段是否包含 {} 字段 （"{}"）'.format(keywords[i], v))
+            flag = False
+    return flag
 
 def open_excel_with_key(file: str, key: str) -> DataFrame:
     '''
@@ -63,15 +78,15 @@ def open_excel_with_key(file: str, key: str) -> DataFrame:
             excel = OfficeFile(f)
             excel.load_key(key)
             excel.decrypt(io_file)
-    i_df = read_excel(io=io_file, sheet_name=READ_SHEET_NAME, dtype={'接收时间': str})
+    i_df = read_excel(io=io_file, sheet_name=READ_SHEET_NAME, dtype={COL_NAME_TIME: str})
     del io_file
     return i_df
 
 
-def init_table(filename: str, col_names: list) -> None:
+def init_table_from_empty(filename: str, col_names: list) -> None:
     '''
     台账初始化
-    若不存在台账，则创建
+    按列名创建一个空台账
     '''
     df = DataFrame({}, columns=col_names)
     df.to_excel(filename, index=False)
@@ -84,7 +99,7 @@ def self_compare(book: DataFrame) -> DataFrame:
     '''
     book.sort_values(by=FIRST_TIME, inplace=True)
     # 统计重复次数
-    dup_series = book[INDEX].value_counts().rename('次数')
+    dup_series = book[INDEX].value_counts().rename(COUNT)
     # 去重，保留最近时间
     book_last = book.drop_duplicates(subset=INDEX, keep='last')[[INDEX, FIRST_TIME]]
     book_last.rename(columns={FIRST_TIME: LAST_TIME}, inplace=True)
@@ -110,7 +125,7 @@ def df_compare(book: DataFrame, df: DataFrame) -> DataFrame:
     df['include'] = df.index.isin(book.index)
     # 包含的更新，次数相加
     df_dup = df[df['include']]
-    book['次数'] = book['次数'].add(df_dup['次数'], fill_value=0)
+    book[COUNT] = book[COUNT].add(df_dup[COUNT], fill_value=0)
     book.update(df_dup[LAST_TIME])
     # 不包含的新增
     _df = df[~df['include']].drop(labels='include', axis=1)
@@ -118,20 +133,39 @@ def df_compare(book: DataFrame, df: DataFrame) -> DataFrame:
     return book
 
 
+def backup_book(book_file):
+    '''
+    先复制，备份台账文件
+    '''
+    _name, _suffix = book_file.split('.')
+    _time = '{:.0f}'.format(time.time())
+    if '1' == BACKUP_MODE:
+        _bak_name = '{}_bak-{}.{}'.format(_name, _time, _suffix)
+    else:
+        _bak_name = '{}_bak.{}'.format( _name, _suffix)
+    _script = 'copy {} {} /y'.format(book_file, _bak_name)
+    os.system(_script)
+    print('>>> 台账已备份为: ', Fore.LIGHTBLUE_EX + _bak_name)
+
+
 def main():
-    print(Fore.LIGHTBLUE_EX + '----------------------------------')
-    print(Fore.CYAN + '{} {}'.format(__title__, __version__))
-    print('**** 作者： {} ****'.format(__author__))
-    print(Fore.LIGHTBLUE_EX + '----------------------------------')
+    print(Fore.LIGHTBLUE_EX + '╭---------------------------------------╮')
+    print(Fore.CYAN + '| {} {} |'.format(__title__, __version__))
+    print('|          **** 作者： {} **** |'.format(__author__))
+    print(Fore.LIGHTBLUE_EX + '└---------------------------------------┘')
+    if not check_config():
+        exit('退出运行..')
     book_file = config.get('common', 'path')
+    is_first_run = False
     if not Path(book_file).is_file():
         # 配置文件不存在
         # print(Fore.RED + '台账文件路径不存在，请修改配置文件 {}，然后重新运行'.format(configfile))
         print(Fore.RED + '台账文件路径不存在，将自动生成：{}'.format(book_file))
-        init_table(book_file, col_names)
+        init_table_from_empty(book_file, COL_NAMES)
+        is_first_run = True
 
     book_df = read_excel(io=book_file, sheet_name=BOOK_SHEET_NAME, index_col=INDEX, dtype={FIRST_TIME: str, LAST_TIME: str})
-    # book_list = book_df[book_df['省份'] == '辽宁'][INDEX].to_list()
+    # book_list = book_df[book_df['省份'] == PROVINCE][INDEX].to_list()
     filename_keyword = config.get('common', 'import_filename')
     filenames = [f for f in Path.cwd().glob('*{}*'.format(filename_keyword)) if Path(f).is_file() if '~$' not in str(f)]
     if filenames == []:
@@ -144,34 +178,37 @@ def main():
     for f in filenames:
         key = config.get('common', 'key')
         i_df = open_excel_with_key(str(f), key)
-        i_df_filter = i_df[i_df['省份'] == '辽宁']
+        i_df_filter = i_df[i_df['省份'] == PROVINCE]
         df_list.append(i_df_filter)
 
         print('')
     # 连接成一个 dataFrame
     df = concat(objs=df_list, ignore_index=True)
     # 修改导入数据的列名，与台账的列名一致
-    df.rename(columns={'接收时间': FIRST_TIME}, inplace=True)
+    df.rename(columns={COL_NAME_TIME: FIRST_TIME}, inplace=True)
     if book_df.empty:
         book_df = concat([book_df, df])
         book_df = self_compare(book_df)
-        print(Fore.MAGENTA + Back.LIGHTYELLOW_EX + '>>>>>self_compare')
+        print(Fore.MAGENTA + Back.LIGHTYELLOW_EX + '>>>>> self_compare')
     else:
         df[LAST_TIME] = ''
-        df['次数'] = None
+        df[COUNT] = None
         book_df = df_compare(book_df, df)
-        print(Fore.MAGENTA + Back.LIGHTYELLOW_EX + '>>>>>df_compare')
+        print(Fore.MAGENTA + Back.LIGHTYELLOW_EX + '>>>>> df_compare')
+
+    if not is_first_run:
+        backup_book(book_file)
 
     book_df.to_excel(excel_writer=book_file, sheet_name=BOOK_SHEET_NAME)
-    # book_df.to_excel(excel_writer=book_file, sheet_name=BOOK_SHEET_NAME, columns=col_names)
+    # book_df.to_excel(excel_writer=book_file, sheet_name=BOOK_SHEET_NAME, columns=COL_NAMES)
 
     end_time = time.time()
     run_time = end_time - start_time
-    print(Fore.GREEN + 'ok. Time used: {:.2f} s'.format(run_time))
+    print(Fore.GREEN + '运行成功结束. 总用时: {:.2f} s'.format(run_time))
 
 
 if __name__ == '__main__':
-    if time.time() > time.mktime(time.strptime('20221031', '%Y%m%d')):
+    if time.time() > time.mktime(time.strptime('20221130', '%Y%m%d')):
         print(Fore.RED + '**** 本工具为旧版本，已无法使用。')
         input()
         exit()
@@ -180,10 +217,6 @@ if __name__ == '__main__':
     except Exception as err:
         print(err)
         traceback.print_exc(file=open(DEBUG_FILE, 'w'))
-        print(Fore.LIGHTRED_EX + '出错啦。请反馈目录中的文件：', DEBUG_FILE)
+        print(Fore.LIGHTRED_EX + '** 出错啦。请反馈目录中的文件：', DEBUG_FILE)
         input('按回车退出')
     exit()
-
-d1 = DataFrame({'a': [10, 11, 12, 13, 14], 'b': [20, 21, 22, 23, 24]})
-d2 = DataFrame({'a': [10, 11, 12, 13, 14], 'b': [20, 21, 22, 23, 24], 'c': [220, 221, 222, 223, 224]})
-# s = Series([5, 6, 7, 8, 9], name='gg')
